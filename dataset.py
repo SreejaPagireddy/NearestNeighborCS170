1cb229304d5503a23d08ab799123d120e69f3ea2
Sreeja Pagireddy <sreeja@karibu.ai>
2025-03-01 10:55:48 -0800

    work

--- dataset.py
+++ dataset.py
@@ -2,6 +2,7 @@
 #lets first do the inputs
 import math
 import shutil
+import random
 # def leave_one_out_cross_validation(data, features, value):
 
 #     #make a copy of the dataset, use a for loop and if its not the part of the feature, make it equal to 0
@@ -32,44 +33,49 @@ import shutil
 #     accuracy = number_correctly_classified / len(data)
 #     return accuracy
 
+# def leave_one_out_cross_validation(data, features, value):
+#     data_size = len(data[1:])
+#     for k in range(data_size):
+#         object_to_classify = data[k][1:]
+#         label_object_to_classify = data[k][0] #
+#         text = f'Looping over k is at the, {k}, location'
+#         print(text)
+#         text = f'The {k} th object is in class, {label_object_to_classify}'
+#         print(text)
+
 def leave_one_out_cross_validation(data, features, value):
-    data_size = len(data[1:])
-    for k in range(data_size):
-        object_to_classify = data[k][1:]
-        label_object_to_classify = data[k][0] #
-        text = f'Looping over k is at the, {k}, location'
-        print(text)
-        text = f'The {k} th object is in class, {label_object_to_classify}'
-        print(text)
-
-
+     random.seed(5)
+     list_values = [0.6, 0.3, 0.9, 0.7, 0.2]
+     value = random.choice(list_values)
+     return value
 
 def feature_search_demo(data):
-    num_feature = len(data[0])-1
+    num_levels = len(data[0]) #going through the columns, removed the -1
     current_set_of_features = []
 
-    for x in range(1,num_feature):
+    for x in range(1,num_levels):
         text = f'On the {x} th level of the search tree'
         print(text)
         feature_to_add_at_this_level = []
         best_so_far_accuracy = 0
-        for k in range(1,num_feature):
-            #if k not in current_set_of_features:    
+        for k in range(1,num_levels):
+            if k not in current_set_of_features:    
                 text = f'--Considering adding the {k} feature'
                 print(text)
-                # accuracy = leave_one_out_cross_validation(data, current_set_of_features, k+1)
+                accuracy = leave_one_out_cross_validation(data, current_set_of_features, k+1)
 
-                # if accuracy > best_so_far_accuracy:
-                #     best_so_far_accuracy = accuracy
-                #     feature_to_add_at_this_level = k
-        #current_set_of_features.append(feature_to_add_at_this_level)
-        #text = f'On level {x} i added feature {feature_to_add_at_this_level} to current set'
+                if accuracy > best_so_far_accuracy:
+                     best_so_far_accuracy = accuracy
+                     feature_to_add_at_this_level = k
+        current_set_of_features.append(feature_to_add_at_this_level)
+        text = f'On level {x} i added feature {feature_to_add_at_this_level} to current set'
+        print(text)
 
 def main():
     open_file = open("dataset1.txt")
     data = open_file.readlines()
     data = [[float(x) for x in row.strip().split("  ")] for row in data]
-    #feature_search_demo(data)
-    leave_one_out_cross_validation(data, 1, 2)
+    feature_search_demo(data)
+    #leave_one_out_cross_validation(data, 1, 2)
 
 main()
